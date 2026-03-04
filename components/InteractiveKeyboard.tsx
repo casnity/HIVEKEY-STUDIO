2bc47cd029bf0879668fd357fae7b248d8eef2d0
Casnity <divyansh917906@gmail.com>
2026-03-04 13:33:17 +0530

    fix(InteractiveKeyboard): Disable snap for smoother interaction
    Disabling the snap behavior in PresentationControls allows for more fluid and natural camera manipulation when interacting with the keyboard model. This improves the overall user experience by removing abrupt movements.

--- components/InteractiveKeyboard.tsx
+++ components/InteractiveKeyboard.tsx
@@ -213,7 +213,7 @@ export function InteractiveKeyboard() {
           
           <PresentationControls
             global
-            snap={true}
+            snap={false}
             rotation={[0, -0.5, 0]}
             polar={[-Math.PI / 3, Math.PI / 3]}
             azimuth={[-Math.PI / 1.4, Math.PI / 2]}
